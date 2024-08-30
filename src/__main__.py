765c4d5a6e27f08025e8c8ff346cbe418c3d6bfa
octaviiian <octavian@phenomic.ai>
2024-08-30 14:54:17 +0000

    extract __main__ clause

---
+++ src/__main__.py
@@ -0,0 +1,14 @@
+import argparse
+from pai_embeddings import PaiEmbeddings
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--results-dir', default="tmp/results/", type=str, help="")
+    parser.add_argument('--h5ad-path', required=True, type=str, help="")
+    parser.add_argument('--tissue-organ', required=True, type=str, help="")
+
+    args = parser.parse_args()
+
+    pai_embeddings = PaiEmbeddings(args.results_dir)
+    pai_embeddings.inference(args.h5ad_path, args.tissue_organ)
--- src/pai_embeddings.py
+++ src/pai_embeddings.py
@@ -3,7 +3,6 @@ import zipfile
 import time
 import logging
 import os
-import argparse
 
 from dotenv import load_dotenv
 import requests
@@ -125,16 +124,3 @@ class PaiEmbeddings:
             zip_ref.extractall(results_dir)
 
         # TODO consider option to cleanup zip file
-
-            
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--results-dir', default="tmp/results/", type=str, help="")
-    parser.add_argument('--h5ad-path', required=True, type=str, help="")
-    parser.add_argument('--tissue-organ', required=True, type=str, help="")
-
-    args = parser.parse_args()
-
-    pai_embeddings = PaiEmbeddings(args.results_dir)
-    pai_embeddings.inference(args.h5ad_path, args.tissue_organ)
