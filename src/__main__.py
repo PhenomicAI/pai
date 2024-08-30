53eaf97f52b5d63dd86821815c0c662885a86f64
octaviiian <octavian@phenomic.ai>
2024-08-30 15:41:07 +0000

    add argparse sub_parser embed

--- src/__main__.py
+++ src/__main__.py
@@ -3,12 +3,16 @@ from embed import PaiEmbeddings
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--results-dir', default="tmp/results/", type=str, help="")
-    parser.add_argument('--h5ad-path', required=True, type=str, help="")
-    parser.add_argument('--tissue-organ', required=True, type=str, help="")
+    parser = argparse.ArgumentParser("PAI (Phenomic AI) CLI")
+    sub_parser = parser.add_subparsers(dest="command")
+
+    embed = sub_parser.add_parser("embed")
+    embed.add_argument('--results-dir', default="tmp/results/", type=str, help="")
+    embed.add_argument('--h5ad-path', required=True, type=str, help="")
+    embed.add_argument('--tissue-organ', required=True, type=str, help="")
 
     args = parser.parse_args()
 
-    pai_embeddings = PaiEmbeddings(args.results_dir)
-    pai_embeddings.inference(args.h5ad_path, args.tissue_organ)
+    if args.command == "embed":
+        pai_embeddings = PaiEmbeddings(args.results_dir)
+        pai_embeddings.inference(args.h5ad_path, args.tissue_organ)
