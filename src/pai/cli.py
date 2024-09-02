93e60c00349b1bc881c3914688dbd6cceec89fd6
octaviiian <octavian@phenomic.ai>
2024-09-02 15:48:40 +0000

    refactor cli code

--- src/pai/cli.py
+++ src/pai/cli.py
@@ -1,8 +1,8 @@
 import argparse
-from src.pai.embed import PaiEmbeddings
+from .embed import PaiEmbeddings
 
-if __name__ == "__main__":
 
+def app():
     parser = argparse.ArgumentParser("PAI (Phenomic AI) CLI")
     sub_parser = parser.add_subparsers(dest="command")
 
@@ -16,3 +16,7 @@ if __name__ == "__main__":
     if args.command == "embed":
         pai_embeddings = PaiEmbeddings(args.results_dir)
         pai_embeddings.inference(args.h5ad_path, args.tissue_organ)
+
+
+if __name__ == "__main__":
+    app()
