742b6560fa98a0254cc2a7a317d987cf1303f2be
octaviiian <octavian@phenomic.ai>
2024-09-03 14:05:00 +0000

    delete comment

--- src/pai/cli.py
+++ src/pai/cli.py
@@ -7,7 +7,7 @@ def app():
     sub_parser = parser.add_subparsers(dest="command")
 
     embed = sub_parser.add_parser("embed")
-    embed.add_argument('--tmp-dir', required=True, type=str, help="")  # /home/ubuntu/tmp/pai/embed
+    embed.add_argument('--tmp-dir', required=True, type=str, help="")
     embed.add_argument('--h5ad-path', required=True, type=str, help="")
     embed.add_argument('--tissue-organ', required=True, type=str, help="")
 
